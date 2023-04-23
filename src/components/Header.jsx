989173abaac51aa80a862f87a6d06f0dd8616b72
Gazi-Farhana <gazifarhana95@gmail.com>
2023-04-24 01:06:45 +0600

    breaktime

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+const {user} = useContext(AuthContext);
+
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
                 <a className="btn btn-ghost normal-case text-xl">FireBase</a>
                 <Link to="/">Home</Link>
                 <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                <Link className='btn btn-ghost normal-case text-xl' to="/register">Register</Link>
+                {
+                    user? <>
+                   <span>{user,email}</span>
+                   <button className = 'btn btn-xs'>Sign Out</button>
+                   </> : <Link to ="/login">Login</Link>
+                }
             </div>
         </div>
     );
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,4 +1,4 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, updateCurrentUser } from "firebase/auth";
 import React, { createContext, useState } from 'react';
 import app from '../firebase/firebase.config';
 
@@ -12,8 +12,29 @@ const user = {displayName: 'sagor nodi'}
 const AuthProviders = ({children}) => {
     const [user, setUser] = useState(null);
 
+    
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+// observe auth state change
+    useEffect = ( () => {
+       const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser);
+            setUser(currentUser);
+        });
+        return () =>{
+            unsubscribe()
+        }
+    }, [])
+
     const authInfo = {
         user,
+        createUser,
+        signIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
